2343075a93b9ffac31704bab8536de43f7676773
csfx-py <csfxshreyas@gmail.com>
2024-03-02 01:03:50 +0530

    configured routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,28 @@
+import { useContext } from "react";
+import { Navigate, Route, Routes, useLocation } from "react-router-dom";
 import "./App.css";
-import Flow from "./Components/Flow";
 import Nav from "./Components/Nav";
-import SideBar from "./Components/SideBar";
-import { NodeProvider } from "./Contexts/NodeContext";
+import { AuthContext } from "./Contexts/AuthContext";
+import Auth from "./Pages/Auth";
+import Flows from "./Pages/Flows";
 
 function App() {
+  const { user } = useContext(AuthContext);
+
+  const { pathname, state } = useLocation();
   return (
     <div className="flex flex-col h-screen w-screen">
       <Nav />
-      <div className="flex h-full">
-        <NodeProvider>
-          <SideBar />
-          <Flow />
-        </NodeProvider>
-      </div>
+      <Routes>
+        <Route
+          path="/"
+          element={user ? <Flows /> : <Navigate to="/auth" state={pathname} />}
+        />
+        <Route
+          path="/auth"
+          element={user ? <Navigate to={state} /> : <Auth />}
+        />
+      </Routes>
     </div>
   );
 }
